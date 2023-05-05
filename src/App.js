b9c3e3c802dc20dd64a97dd2d86ef1feaad2a8c4
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-05-04 21:28:05 -0500

    All components importing properly from their respective folders and displaying well

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react'
 
 import './App.css';
-
-// import Article from './components/article/Article';
-// import Brand from './components/brand/Brand';
-// import Feature from './components/feature/Feature';
-// import CTA from './components/cta/CTA';
-// import Navbar from './components/navbar/Navbar';
+import {Footer, Features, Possibility, Header, WhatGPT3, Blog} from './containers';
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import Feature from './components/feature/Feature';
+import CTA from './components/cta/CTA';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
-      <h1>GPT-3</h1>
-      <h1>GPT-3</h1>
-      <h1>GPT-3</h1>
-      <h1>GPT-3</h1>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
