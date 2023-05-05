aeb907b7e77daa1c33724ff79a7ba460f29e0277
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-05-04 21:16:48 -0500

    Added folder structure that inl's all relevant jsx and css files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import './App.css';
+
+// import Article from './components/article/Article';
+// import Brand from './components/brand/Brand';
+// import Feature from './components/feature/Feature';
+// import CTA from './components/cta/CTA';
+// import Navbar from './components/navbar/Navbar';
+
 const App = () => {
   return (
     <div>
-        <h1>This is FRANKENMILLER's website</h1>
+      <h1>GPT-3</h1>
+      <h1>GPT-3</h1>
+      <h1>GPT-3</h1>
+      <h1>GPT-3</h1>
+      <h1>GPT-3</h1>
+      <h1>GPT-3</h1>
     </div>
   )
 }
