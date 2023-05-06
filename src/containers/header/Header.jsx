a8fa0da69b0d4e0b78c5a1b10b96bf5b2483494b
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-05-06 18:10:19 -0500

    All text and button styling complete

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -11,18 +11,18 @@ const Header = () => {
         <h1 className='gradient__text'>
           Let's Build Something Amazing with GPT-3 OpenAI
         </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <div className='gpt3__header-content__input'>
           <input type="email" placeholder='Your Email Address' />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           <button type='button'>Get Started</button>
         </div>
         <div className='gpt3__header-content__people'>
           <img src={people} alt="bubble img's for ppl's faces" />
           <p>1600 people have visited in the last 24 hours</p>
         </div>
-        <div className='gpt3__header-image'>
+        {/* <div className='gpt3__header-image'>
           <img src={ai} alt="creepy looking ai generated face" />
-        </div>
+        </div> */}
       </div>
     </div>
   )
