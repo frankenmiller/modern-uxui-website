9d8b79df629f7d1662259b57c07f7831a726f801
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-05-06 21:20:19 -0500

    added code for brand component, all img's displaying and properly centered

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {google, atlassian, shopify, dropbox, slack} from "./imports";
 import './brand.css';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google img" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian img" />
+      </div>
+      <div>
+        <img src={slack} alt="slack img" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify img" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox img" />
+      </div>
+    </div>
   )
 }
 
